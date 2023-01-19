016e876b211ef26a6660ebdfb2ba16d9453b87b0
devasuresh <devasuresh93@gmail.com>
2023-01-19 18:00:36 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,6 @@ module "s3-bucket" {
   source  = "app.terraform.io/pearson-pif-terraform-poc/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
   acl    = "private"
   bucket_prefix = var.prefix
   versioning = {
